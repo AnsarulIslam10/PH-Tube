d48ae3ff168efa159a4e58f70d8d12ef8018add9
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-03 08:38:58 +0600

    active button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ function getTimeString(time){
     const minute = parseInt(remainingSecond / 60);
     return `${hour}hrs ${minute} min ago`
 }
+
+const removeActiveClass = () =>{
+  const buttons = document.getElementsByClassName('category-btn');
+  for (const btn of buttons) {
+    btn.classList.remove("active");
+  }
+}
 // create loadCategories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -30,7 +37,7 @@ const displayCategories = (categories) => {
     const buttonContainer = document.createElement("div");
     buttonContainer.innerHTML = 
     `
-    <button onclick="loadCategoryVideos(${item.category_id})" class="btn">
+    <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
     ${item.category}
     </button>
     
@@ -44,7 +51,12 @@ const displayCategories = (categories) => {
 const loadCategoryVideos = (id) =>{
     fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
     .then((res) => res.json())
-    .then((data) => displayVideos(data.category))
+    .then((data) => {
+      removeActiveClass()
+      const activeBtn = document.getElementById(`btn-${id}`)
+      activeBtn.classList.add("active")
+      displayVideos(data.category)
+    })
     .catch((error) => console.log(error));
 }
 // demo card
